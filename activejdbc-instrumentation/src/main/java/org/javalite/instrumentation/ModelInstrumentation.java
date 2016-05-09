fddbc19ac16731f18470d65bf2f8f209863eac35
Jarosław Śmigielski <jaroslaw.smigielski@interia.pl>
2016-05-10 00:05:33 +0200

    Working version of repository instrumentation

--- activejdbc-instrumentation/src/main/java/org/javalite/instrumentation/ModelInstrumentation.java
+++ activejdbc-instrumentation/src/main/java/org/javalite/instrumentation/ModelInstrumentation.java
@@ -20,8 +20,11 @@ package org.javalite.instrumentation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
 import javassist.*;
 import javassist.bytecode.SignatureAttribute;
+import org.apache.log4j.Logger;
+import org.javalite.activejdbc.annotations.InstrumentWith;
 
 /**
  * @author Igor Polevoy
@@ -32,7 +35,7 @@ public class ModelInstrumentation {
     private final CtClass modelClass;
     
     private final List<String> instrumentationSources = Arrays.asList("org.javalite.activejdbc.Model","pl.hereworx.activeRepository.GenericRepository");
-
+    
     public ModelInstrumentation() throws NotFoundException {
         ClassPool cp = ClassPool.getDefault();
         cp.insertClassPath(new ClassClassPath(this.getClass()));
@@ -52,6 +55,9 @@ public class ModelInstrumentation {
     private void doInstrumentFromClass(CtClass target,CtClass source) throws NotFoundException, CannotCompileException {
         CtMethod[] modelMethods = source.getDeclaredMethods();
         CtMethod[] targetMethods = target.getDeclaredMethods();
+        
+        CtField[] modelFields = source.getDeclaredFields();
+        CtField[] targetFields = target.getDeclaredFields();
 
         CtMethod newGetClass = null;
         CodeConverter conv = new CodeConverter();
@@ -66,6 +72,8 @@ public class ModelInstrumentation {
             conv.redirectMethodCall(modelGetClass, newGetClass);
             classMap.fix(source);
         }
+        
+
         
         // do not convert Model class to Target class in methods
         
@@ -99,6 +107,32 @@ public class ModelInstrumentation {
                 }
             }
         }
+        
+        for (CtField field: modelFields){
+                instrumentField(field, target);
+        }
+    }
+
+    private void instrumentField(CtField field, CtClass target) throws CannotCompileException {
+        
+        InstrumentWith annotation;
+        try {
+            annotation = (InstrumentWith) field.getAnnotation(InstrumentWith.class);
+        } catch (ClassNotFoundException ex) {
+            return;
+        }
+        if (annotation == null){
+            return;
+        }
+        CtField newField = new CtField(field, target);
+        String initializer = annotation.initialize();
+        
+        if (!"".equals(initializer)){
+            System.out.println("Adding field ["+field.getName()+"] with initializer method["+initializer+"]");
+            target.addField(newField, CtField.Initializer.byCall(target, initializer));
+        } else {
+            target.addField(newField);
+        }
     }
 
     private boolean targetHasMethod(CtMethod[] targetMethods, CtMethod delegate) {
@@ -112,7 +146,9 @@ public class ModelInstrumentation {
 
     private void doInstrument(CtClass target) throws NotFoundException, CannotCompileException {
         CtClass[] sources = getInstrumentationSources(target);
+        System.out.println("Number of instrumetation sources: "+sources.length);
         for (CtClass source: sources){
+            System.out.println("Processing ["+source.getName()+"]");
             doInstrumentFromClass(target, source);
         }
     }
@@ -132,6 +168,141 @@ public class ModelInstrumentation {
 
     private boolean validInstrumentationSource(CtClass sc) {
         return instrumentationSources.contains(sc.getName());
+    }
+
+    private boolean fieldToInstrument(CtField field) {
+        try {
+            return field.getAnnotation(InstrumentWith.class) != null;
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Annotation class not found");
+            return false;
+        }
+    }
+    
+    private interface  Instrumenter {
+        public void doInstrumentFromClass(CtClass target,CtClass source) throws NotFoundException, CannotCompileException;
+    }
+    
+    private class InstrumentFromModel implements Instrumenter {
+        @Override
+        public void doInstrumentFromClass(CtClass target, CtClass source) throws NotFoundException, CannotCompileException {
+            CtMethod[] modelMethods = source.getDeclaredMethods();
+            CtMethod[] targetMethods = target.getDeclaredMethods();
+
+            CtMethod newGetClass = null;
+            CodeConverter conv = new CodeConverter();
+            ClassMap classMap = new ClassMap();
+
+            if (source.equals(modelClass)){
+                CtMethod modelGetClass = source.getDeclaredMethod("modelClass");
+                newGetClass = CtNewMethod.copy(modelGetClass, target, null);
+                newGetClass.setBody("{ return " + target.getName() + ".class; }");
+                // convert Model.getDaClass() calls to Target.getDaClass() calls
+                conv.redirectMethodCall(modelGetClass, newGetClass);
+                classMap.fix(source);
+            }
+
+            // do not convert Model class to Target class in methods
+
+            for (CtMethod method : modelMethods) {
+                int modifiers = method.getModifiers();
+                if (Modifier.isStatic(modifiers)) {
+                    if (targetHasMethod(targetMethods, method)) {
+                        Instrumentation.log("Detected method: " + method.getName() + ", skipping delegate.");
+                    } else {
+                        CtMethod newMethod;
+                        if ((Modifier.isProtected(modifiers) || Modifier.isPublic(modifiers)) && !"modelClass".equals(method.getName()) ) {
+                            newMethod = CtNewMethod.copy(method, target, classMap);
+                            newMethod.instrument(conv);
+                        } else if ("modelClass".equals(method.getName())) {
+                            if (newGetClass!=null){
+                                newMethod = newGetClass;
+                            } else {
+                                newMethod = CtNewMethod.copy(method, target, classMap);
+                            }
+                        } else {
+                            newMethod = CtNewMethod.delegator(method, target);
+                        }
+
+                        // Include the generic signature
+                        for (Object attr : method.getMethodInfo().getAttributes()) {
+                            if (attr instanceof SignatureAttribute) {
+                                newMethod.getMethodInfo().addAttribute((SignatureAttribute) attr);
+                            }
+                        }
+                        target.addMethod(newMethod);
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    private class InstrumentFromGenericRepository implements Instrumenter {
+        @Override
+        public void doInstrumentFromClass(CtClass target, CtClass source) throws NotFoundException, CannotCompileException {
+            CtMethod[] modelMethods = source.getDeclaredMethods();
+            CtMethod[] targetMethods = target.getDeclaredMethods();
+
+            CtField[] modelFields = source.getDeclaredFields();
+            CtField[] targetFields = target.getDeclaredFields();
+            
+            CtMethod newGetClass = null;
+            CodeConverter conv = new CodeConverter();
+            ClassMap classMap = new ClassMap();
+
+            /*if (source.equals(modelClass)){
+                CtMethod modelGetClass = source.getDeclaredMethod("modelClass");
+                newGetClass = CtNewMethod.copy(modelGetClass, target, null);
+                newGetClass.setBody("{ return " + target.getName() + ".class; }");
+                // convert Model.getDaClass() calls to Target.getDaClass() calls
+                conv.redirectMethodCall(modelGetClass, newGetClass);
+                classMap.fix(source);
+            }
+            */
+            // do not convert Model class to Target class in methods
+            
+        /*    for (CtField field: modelFields){
+                if (this.fieldToInstrument(field)){
+                    CtField newField = new CtField(field, target);
+                    target.addField(newField);
+                }
+            }*/
+            
+            for (CtMethod method : modelMethods) {
+                int modifiers = method.getModifiers();
+                if (Modifier.isStatic(modifiers)) {
+                    if (targetHasMethod(targetMethods, method)) {
+                        Instrumentation.log("Detected method: " + method.getName() + ", skipping delegate.");
+                    } else {
+                        CtMethod newMethod;
+                        if ((Modifier.isProtected(modifiers) || Modifier.isPublic(modifiers)) && !"modelClass".equals(method.getName()) ) {
+                            newMethod = CtNewMethod.copy(method, target, classMap);
+                            newMethod.instrument(conv);
+                        } else if ("modelClass".equals(method.getName())) {
+                            if (newGetClass!=null){
+                                newMethod = newGetClass;
+                            } else {
+                                newMethod = CtNewMethod.copy(method, target, classMap);
+                            }
+                        } else {
+                            newMethod = CtNewMethod.delegator(method, target);
+                        }
+
+                        // Include the generic signature
+                        for (Object attr : method.getMethodInfo().getAttributes()) {
+                            if (attr instanceof SignatureAttribute) {
+                                newMethod.getMethodInfo().addAttribute((SignatureAttribute) attr);
+                            }
+                        }
+                        target.addMethod(newMethod);
+                    }
+                }
+            }
+        }
+
+
+        
     }
 
 }
